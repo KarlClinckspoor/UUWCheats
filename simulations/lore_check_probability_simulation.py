46e59520ab2577f8dcec7b32e4df795383f4c262
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2024-06-05 20:35:34 -0300

    Made more simulations

--- simulations/lore_check_probability_simulation.py
+++ simulations/lore_check_probability_simulation.py
@@ -17,12 +17,12 @@ def skill_check(player_skill, magic_number_difficulty):
         result = 2
     return result
 
-def lore_check(player_skill):
-    return skill_check(player_skill, 8)
+def lore_check(player_skill, magic_number_difficulty = 8):
+    return skill_check(player_skill, magic_number_difficulty)
 
 def simulations():
     lores = list(range(1, 31))
-    tries = 10000
+    tries = 1000000
     counts = []
     ratios = []
     for lore in lores:
@@ -50,6 +50,56 @@ def plot_simulation(skills, counts, ratios):
     y1 = [i['critical_success']*100 for i in ratios]
     y2 = [i['success']*100 for i in ratios]
     y3 = [i['corrected_failure']*100 for i in ratios]
+    plt.plot(x, y1, label='Fully identified', marker='o', lw=0)
+    plt.plot(x, y2, label='Partially identified', marker='o', lw=0)
+    plt.plot(x, y3, label='Unidentified', marker='o', lw=0)
+    # plt.xticks(range(0, 31, 1), range(0, 31, 5))
+    plt.xlabel('Player lore skill')
+    plt.ylabel('Percentage')
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig('vanilla_lore_probability.png', dpi=300)
+
+if __name__ == '__main__':
+    plot_simulation(*simulations())
+
+def simulations2():
+    magic_numbers = list(range(0, 15))
+    collection = []
+    for magic_number in magic_numbers:
+        lores = list(range(1, 31))
+        tries = 10000
+        counts = []
+        ratios = []
+        for lore in lores:
+            results = []
+            for i in range(tries):
+                results.append(lore_check(lore, magic_number))
+            c = Counter(results)
+            counts.append(c)
+            r = {}
+            r['critical_failure'] = c.get(-1, 0) / tries
+            r['failure'] = c.get(0, 0) / tries
+            r['corrected_failure'] = (c.get(-1, 0) + c.get(0, 0)) / tries
+            r['success'] = c.get(1, 0) / tries
+            r['critical_success'] = c.get(2, 0) / tries
+            ratios.append(r)
+        with open(f'lore_simulation_magic{magic_number}.txt', 'w') as fhand:
+            for lore, count, ratio in zip(lores, counts, ratios):
+                fhand.write(str(lore) + ' ' + str(count) + ' ' + str(ratio) + '\n')
+
+        collection.append((magic_number, lores, counts, ratios))
+
+    return collection
+
+def plot_simulation2(magic_number, skills, counts, ratios):
+    x = skills
+    y1 = [i['critical_success']*100 for i in ratios]
+    y2 = [i['success']*100 for i in ratios]
+    y3 = [i['corrected_failure']*100 for i in ratios]
+    plt.figure()
+    plt.title(magic_number)
     plt.plot(x, y1, label='Fully identified')
     plt.plot(x, y2, label='Partially identified')
     plt.plot(x, y3, label='Unidentified')
@@ -57,10 +107,15 @@ def plot_simulation(skills, counts, ratios):
     plt.ylabel('Percentage')
     plt.legend()
     plt.tight_layout()
-    plt.savefig('vanilla_lore_probability.png', dpi=300)
+    plt.savefig(f'lore_probability_magic{magic_number}.png', dpi=300)
 
+# if __name__ == '__main__':
+#     plot_simulation(*simulations())
 if __name__ == '__main__':
-    plot_simulation(*simulations())
+    c = simulations2()
+    for magic_number, skills, counts, ratios in c:
+        plot_simulation2(magic_number, skills, counts, ratios)
+
 
 
 

82a6b258cc6b408a256b3f32eed2130b09fe61b1
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2024-07-14 19:03:17 -0300

    Updated the patcher with a diff instance, removing requirement of consecutive patches.

--- pythonpatcher/patcher.py
+++ pythonpatcher/patcher.py
@@ -1,52 +1,60 @@
+from pathlib import Path
+
+
+class Diff:
+    def __init__(self, offset: int | str, original: int | str, new: int | str):
+        self.offset = offset if isinstance(offset, int) else int(offset, 16)
+        self.original = original if isinstance(original, int) else int(original, 16)
+        self.new = new if isinstance(new, int) else int(new, 16)
+
+    def to_json(self):
+        return f'{{"offset": {self.offset}, "original": {self.original}, "new": {self.new}}}'
+
+    @classmethod
+    def from_json(cls, json_content: dict):
+        return cls(**json_content)
+
+
 class Patch:
 
-    def __init__(self, offset: int, original: list[int], new: list[int], meaning: str | None = None):
-        if len(original) != len(new):
-            raise ValueError("original and new must be the same length")
-        self.offset = offset
-        self.original = original
-        self.new = new
+    def __init__(self, diffs: list[Diff], meaning: str | None = None):
+        self.diffs = diffs
         self.meaning = meaning if meaning is not None else ""
 
     def to_json(self) -> str:
-        return f'{{"offset": {self.offset}, "original": {self.original}, "new": {self.new}, "meaning": "{self.meaning}"}}'
+        return '[' + "".join(([diff.to_json() for diff in self.diffs])) + ']'
 
     @classmethod
     def from_json(cls, json_content: dict) -> 'Patch':
         return cls(**json_content)
 
     def check_if_content_is_expected(self, buffer: list[int]):
-        for i, byte in enumerate(buffer[self.offset: self.offset + len(self.original)]):
-            if byte != self.original[i]:
-                return i, byte, False
-        return -1, -1, True
+        for diff in self.diffs:
+            byte_pos = diff.offset
+            byte_val = buffer[byte_pos]
+            if byte_val != diff.original:
+                return byte_pos, byte_val, False
+        return None, None, True
 
     def apply_patch(self, buffer: list[int]):
         byte_pos, byte_val, result = self.check_if_content_is_expected(buffer)
         if not result:
             raise ValueError(
                 f"Found mismatching content at offset {byte_pos} with value {byte_val} (byte {byte_pos}")
-        for i, byte in enumerate(self.new):
-            buffer[self.offset + i] = byte
+        for diff in self.diffs:
+            buffer[diff.offset] = diff.new
 
 
 class Patcher:
-    def __init__(self, original_content: list[int], patches: list[Patch]):
-        self.original_content = original_content
-        self.patched_content = original_content.copy()
+    def __init__(self, original_buffer: list[int], patches: list[Patch]):
+        self.original_buffer = original_buffer
+        self.patched_buffer = original_buffer.copy()
         self.patches = patches
 
     def apply_patches(self):
         for patch in self.patches:
-            patch.apply_patch(self.patched_content)
+            patch.apply_patch(self.patched_buffer)
 
-    def to_json(self) -> str:
-        return str([patch.to_json() for patch in self.patches])
-
-    @classmethod
-    def from_json(cls, json_content: dict) -> 'Patcher':
-        return cls(**json_content)
-
-    def save_to_file(self, filename: str):
-        with open(filename, "wb") as f:
-            f.write(bytes(self.patched_content))
+    def save_to_file(self, filepath: str | Path):
+        with open(filepath, "wb") as f:
+            f.write(bytes(self.patched_buffer))

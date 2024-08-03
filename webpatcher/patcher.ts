70113974b45c5e0895f123cd32a89e09a76b1232
Karl Clinckspoor <30571394+KarlClinckspoor@users.noreply.github.com>
2024-08-03 08:34:58 -0300

    Sketched a method to apply the patches depending on what is checked.

--- webpatcher/patcher.ts
+++ webpatcher/patcher.ts
@@ -1,4 +1,4 @@
-let buffer: Uint8Array;
+var buffer: Uint8Array;
 
 function loadExecutable() {
     // Query for file
@@ -6,23 +6,32 @@ function loadExecutable() {
     // Compute hash
     // Compare with required hash
     var uw2_gog_hash = "BF233ABBFEB5B664564B954FC70C615C4023AD2276DD3326FCD34700C10AFDB9";
-    buffer = new Uint8Array(20);
+    buffer = new Uint8Array(675184);
 }
 
 function applyPatches() {
     // Query all checkboxes
-    // For each checkbox that's checked
-    // Apply that specific function, which might query for other info
+    var inputs = document.getElementsByClassName("options");
+    var selectedInputs: Array<HTMLInputElement> = Array.prototype.filter.call(inputs, (input: Element) => (<HTMLInputElement>input).checked);
+    var functionMaps = {
+        "increase_carry_weight": applyCarryWeightPatch,
+        "allow_sequential_lore_checks": applySequentialLoreChecks,
+        default: () => { console.log("Unknown patch. BUG! REPORT!"); return; }
+    }
+    for (var input of selectedInputs) {
+        functionMaps[input.id](buffer);
+    }
 }
 
-function applySequentialLoreChecks(buffer: Uint8Array) {
+function applySequentialLoreChecks() {
+    if (buffer === undefined) { throw new Error("buffer wasn't loaded!"); }
     if (buffer[0x28DA0] != 0x04) {
         throw new Error("Unexpected value at 'applySequentialLoreChecks'!")
     }
     buffer[0x28DA0] = 0x00;
 }
 
-function applyCarryWeightPatch(buffer: Uint8Array) {
+function applyCarryWeightPatch() {
     // apply checks
     var ele = <HTMLInputElement>document.getElementById("carry_weight_multiplier")!;
     var value = parseFloat(ele.value);
